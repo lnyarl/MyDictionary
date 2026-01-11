a5455b52185e1e21dfb3dbb51e8b1fb1f37cd518
yongjae <lnyarl@gmail.com>
2026-01-11 22:55:03 +0900

    react compile 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,30 @@
 import path from "node:path";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
+const ReactCompilerConfig = {
+	target: "19",
+	compilationMode: "annotation",
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+				babelrc: false,
+				configFile: false,
+			},
+			include: ["**/*.tsx", "**/*.ts"],
+			exclude: [/node_modules/],
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+			react: path.resolve(__dirname, "./node_modules/react"),
+			"react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
 		},
 	},
 	server: {
