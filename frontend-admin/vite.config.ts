8ad76287716444a17dd8d24bdc753448a07444c5
yongjae <lnyarl@gmail.com>
2026-01-15 00:44:11 +0900

    tailwind 버전업 오류 수정

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -1,24 +1,25 @@
-import react from "@vitejs/plugin-react";
 import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-			react: path.resolve(__dirname, "../node_modules/react"),
-			"react-dom": path.resolve(__dirname, "../node_modules/react-dom"),
-		},
-	},
-	server: {
-		port: 5174,
-		proxy: {
-			"/api": {
-				target: "http://localhost:3001",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      react: path.resolve(__dirname, "../node_modules/react"),
+      "react-dom": path.resolve(__dirname, "../node_modules/react-dom"),
+    },
+  },
+  server: {
+    port: 5174,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
