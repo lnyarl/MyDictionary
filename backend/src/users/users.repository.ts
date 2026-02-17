2ae0bc2228627bdee484c1eeb2ecea132cf9e9d9
lnyarl <lnyarl@gmail.com>
2026-02-17 21:51:43 +0900

    tq..

--- backend/src/users/users.repository.ts
+++ backend/src/users/users.repository.ts
@@ -42,7 +42,7 @@ export class UsersRepository extends BaseRepository {
     userId: string,
     updates: { nickname?: string; bio?: string; profilePicture?: string },
   ): Promise<Users> {
-    const dbUpdates: { nickname?: string; bio?: string; profile_picture?: string } = { ...updates };
+    const dbUpdates: { nickname?: string; bio?: string; profile_picture?: string, profilePicture?: string } = { ...updates };
     if (updates.profilePicture) {
       dbUpdates.profile_picture = updates.profilePicture;
       delete dbUpdates.profilePicture;
